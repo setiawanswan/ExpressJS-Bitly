d16fd402605eb3cea5d9ee70f663aa08492ed6c3
setiawan.tumilin@gmail.com <setiawan.tumilin@gmail.com>
2018-09-24 13:33:59 +0700

    first commit

--- models/user.js
+++ models/user.js
@@ -4,7 +4,7 @@
 
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
-    firstName: DataTypes.STRING,
+    firstname: DataTypes.STRING,
     lastName: DataTypes.STRING,
     email: DataTypes.STRING,
     password: DataTypes.STRING
